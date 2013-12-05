40579d66596766549afcd63a47a5e92597256394
David Anderson <davidmartinanderson@gmail.com>
2013-12-04 16:46:06 -0800

    updated the kindrid header signature

--- lib/kindrid_client/validate.rb
+++ lib/kindrid_client/validate.rb
@@ -7,7 +7,7 @@ module KindridClient
       message = "#{request.url}#{request.body.read}"
       digest = OpenSSL::Digest::Digest.new('sha1')
       hmac = OpenSSL::HMAC.hexdigest(digest, KindridClient.key, message)
-      request_hmac = request.headers["XKindridSignature"]
+      request_hmac = request.headers["X-Kindrid-Signature"]
 
       hmac == request_hmac
     end
